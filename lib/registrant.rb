8ce91918eede74897cc0ff8ee62c4c1aec131e0d
Patrick Shim <patrick.shim91@yahoo.com>
2024-10-17 18:53:04 -0400

    added permit?, earn_permit method

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -1,3 +1,20 @@
 class Registrant
+    attr_reader :name, :age, :permit, :license_data
+
+    def initialize(name, age, permit = false, license_data = {:written=>false, :license=>false, :renewed=>false})
+        @name = name
+        @age = age
+        @permit = permit
+        @license_data = license_data
+    end
     
+    def permit?
+        @permit = permit
+    end
+
+    def earn_permit
+        if @permit != true
+            @permit = true
+        end
+    end
 end
